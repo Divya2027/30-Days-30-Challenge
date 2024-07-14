74b15bc52f802a5ad8e4c2e1227fd05393a67592
Divya jha <169071131+Divya2027@users.noreply.github.com>
2024-07-14 17:35:41 +0000

    continune day 2

---
+++ Day_2/Activity_1.js
@@ -0,0 +1,10 @@
+
+
+let a = 8;
+let b = 2;
+console.log(a+b);    // task 1
+console.log(a-b);    // task 2
+console.log(a*b);    // task 3
+console.log(a/b);    // task 4
+console.log(a%b);    // task 5
+
