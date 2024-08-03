88d4b517eea6ad43abc9d9d41df31fd4668afa69
Divya jha <169071131+Divya2027@users.noreply.github.com>
2024-08-03 18:36:28 +0000

    Array done

--- Day_5/Activity_05.js
+++ Day_5/Activity_05.js
@@ -12,11 +12,11 @@ console.log(matrix);
 
 // task 13
 
-const matrices = [
+const matrix2 = [
     [2, 4, 6],
     [3, 6, 9],
     [4, 8, 12]
 ];
-const specificElement = matrices[2][1];
+const specificElement = matrix2[2][1];
 
 console.log(specificElement);
